372a1e282d70e5759f9ad8dae64475e3ac1e935a
ketan purohit <ketanpurohit0@gmail.com>
2020-09-14 21:58:21 +0100

    Changes by black code formatter

--- spark/test_helper.py
+++ spark/test_helper.py
@@ -49,7 +49,7 @@ def df5(spark):
 
 @pytest.fixture
 def df6(spark):
-    dict_lst = {"letters": ["a", "", "b", " "],  "numbers": [1, 2, 3, 4]}
+    dict_lst = {"letters": ["a", "", "b", " "], "numbers": [1, 2, 3, 4]}
 
     column_names, data = zip(*dict_lst.items())
     return spark.createDataFrame(zip(*data), column_names)
@@ -68,8 +68,8 @@ def test_all_same(spark, df1):
     )
     pass_count = dfResult.filter("PASS == True").count()
     overall_count = dfResult.count()
-    assert(pass_count == overall_count)
-    assert(df1.count() == overall_count)
+    assert pass_count == overall_count
+    assert df1.count() == overall_count
 
 
 def test_all_different(spark, df1, df2):
@@ -84,7 +84,7 @@ def test_all_different(spark, df1, df2):
         joinType="full_outer",
     )
     pass_count = dfResult.filter("PASS == True").count()
-    assert(pass_count == 0)
+    assert pass_count == 0
 
 
 def test_partial_same(spark, df1, df3):
@@ -100,8 +100,8 @@ def test_partial_same(spark, df1, df3):
     )
     pass_count = dfResult.filter("PASS == True").count()
     overall_count = dfResult.count()
-    assert(pass_count < overall_count)
-    assert(pass_count > 0)
+    assert pass_count < overall_count
+    assert pass_count > 0
 
 
 def test_no_common(spark, df1, df4):
@@ -116,12 +116,13 @@ def test_no_common(spark, df1, df4):
         joinType="full_outer",
     )
     pass_count = dfResult.filter("PASS == True").count()
-    assert(pass_count == 0)
+    assert pass_count == 0
 
 
 def countNullsAcrossAllColumns(df):
     # https://www.datasciencemadesimple.com/count-of-missing-nanna-and-null-values-in-pyspark/
     from pyspark.sql.functions import isnull, when, count, expr
+
     nullCountDf = df.select([count(when(isnull(c), c)).alias(c) for c in df.columns])
     sumExpr = "+".join(nullCountDf.columns) + " as TOTAL"
     sumDf = nullCountDf.select(expr(sumExpr))
@@ -132,7 +133,9 @@ def countWSAcrossAllStringColumns(df):
     from pyspark.sql.functions import col, when, count, trim, expr
 
     stringCols = [cn for (cn, ct) in df.dtypes if ct == "string"]
-    blanksCountdf = df.select([count(when(trim(col(c)) == "", True)).alias(c) for c in stringCols])
+    blanksCountdf = df.select(
+        [count(when(trim(col(c)) == "", True)).alias(c) for c in stringCols]
+    )
     sumExpr = "+".join(blanksCountdf.columns) + " as TOTAL"
     sumDf = blanksCountdf.select(expr(sumExpr))
     return sumDf.collect()[0].TOTAL
@@ -140,19 +143,19 @@ def countWSAcrossAllStringColumns(df):
 
 def test_null_replacement(spark, df5):
     totalNulls = countNullsAcrossAllColumns(df5)
-    assert(totalNulls > 0)
+    assert totalNulls > 0
 
     # now replace NULLS
     df5 = sh.replaceNulls(df5)
     totalNulls = countNullsAcrossAllColumns(df5)
-    assert(totalNulls == 0)
+    assert totalNulls == 0
 
 
 def test_blank_replacement(spark, df6):
     totalBlanks = countWSAcrossAllStringColumns(df6)
-    assert(totalBlanks == 2)
+    assert totalBlanks == 2
 
     # now replace BLANKS
     df6 = sh.replaceBlanks(df6)
     totalBlanks = countWSAcrossAllStringColumns(df6)
-    assert(totalBlanks == 0)
+    assert totalBlanks == 0
