5022622974b2eb2096dcbe70726d5c3bcea08a7e
ketan purohit <ketanpurohit0@gmail.com>
2020-11-10 15:40:25 +0000

    LeetCode/Hard/420. Strong Password Checker

--- competitions/src/competition.py
+++ competitions/src/competition.py
@@ -4,22 +4,28 @@ class Solution420:
 
     def strongPasswordChecker(self, s: str) -> int:
         list = [Solution420.minCharacters(s), Solution420.domainCheck(s), Solution420.repeatCheck(s)]
-        return list.count(False)
+        fixes = sum(x for (_, x) in list)
+        return fixes
 
     # It has at least 6 characters and at most 20 characters.
-    def minCharacters(s: str) -> bool:
-        return 6 <= len(s) <= 20
+    def minCharacters(s: str):
+        corrections: int = 0
+        if (len(s) < 6):
+            corrections = 6-len(s)
+        elif (len(s) > 20):
+            corrections = len(s) - 20
+        return (6 <= len(s) <= 20, corrections)
 
     # It must contain at least one lowercase letter, at least one uppercase letter, and at least one digit.
-    def domainCheck(s: str) -> bool:
+    def domainCheck(s: str):
         regex: str = ".*[0-9].*"
         import re
         list = [(s.upper() != s), (s.lower() != s), (re.search(regex, s) != None)]
-        return all(list)
+        return (all(list), list.count(False))
 
     # It must NOT contain three repeating characters in a row
     def repeatCheck(s: str) -> bool:
         regex = r"(\w)\1\1"
         import re
-        matches = re.search(regex, s)
-        return (matches is None)
+        matches = re.findall(regex, s)
+        return (len(matches)==0, len(matches))
--- competitions/tests/test_1.py
+++ competitions/tests/test_1.py
@@ -8,11 +8,11 @@ def test_dmmy() -> None:
 
 
 def test_password_len() -> None:
-    pwds = {"tosrt": False,
-            "alllowercase": True,
-            "ALLUPPER": True,
-            "dfddsdsdffdfs": True,
-            "ABBBBBBBBBBBBBBBBBBBBBBBBBBB": False}
+    pwds = {"tosrt": (False, 1),
+            "alllowercase": (True, 0),
+            "ALLUPPER": (True, 0),
+            "dfddsdsdffdfs": (True, 0),
+            "ABBBBBBBBBBBBBBBBBBBBBBBBBBB": (False, 8)}
     for (pwd, expect) in pwds.items():
         # print(pwd, Solution420.minCharacters(pwd))
         assert(expect == Solution420.minCharacters(pwd))
@@ -20,14 +20,14 @@ def test_password_len() -> None:
 
 def test_password_domain_check() -> None:
 
-    pwds = {"tosrt": False,
-            "alllowercase": False,
-            "ALLUPPER": False,
-            "dfddsdsdffdfs": False,
-            "ABBBBBBBBBBBBBBBBBBBBBBBBBBB": False,
-            "aA": False,
-            "aA0": True,
-            "zZ9": True}
+    pwds = {"tosrt": (False, 2),
+            "alllowercase": (False, 2),
+            "ALLUPPER": (False, 2),
+            "dfddsdsdffdfs": (False, 2),
+            "ABBBBBBBBBBBBBBBBBBBBBBBBBBB": (False, 2),
+            "aA": (False, 1),
+            "aA0": (True, 0),
+            "zZ9": (True, 0)}
     for (pwd, expect) in pwds.items():
         assert(expect == Solution420.domainCheck(pwd))
 
@@ -35,42 +35,46 @@ def test_password_domain_check() -> None:
 def test_password_repeats() -> None:
 
     pwds = {
-            "tosrt": True,
-            "ALLUPPER": True,
-            "dfddsdsdffdfs": True,
-            "aA": True,
-            "aA0": True,
-            "zZ9": True}
+            "AAAtosrtZZZ": (False, 2),
+            "ALLUPPER": (True, 0),
+            "dfddsdsdffdfs": (True, 0),
+            "aA": (True, 0),
+            "aA0": (True, 0),
+            "zZ9": (True, 0),
+            "ThisIIISnotaG00dPass": (False, 1),
+            "AAABBBCCC": (False, 3)}
     for (pwd, expect) in pwds.items():
         assert(expect == Solution420.repeatCheck(pwd))
 
 
 def testPasswords() -> None:
 
-    pwds = {"this1sAGoodPass": True,
-            "thisisnotagoodpass": False,
-            "THISISNOTAGOODPASS": False,
-            "ThisIIISnotaG00dPass": False}
+    pwds = {"this1sAGoodPass": (True, 0),
+            "thisisnotagoodpass": (False, 2),
+            "THISISNOTAGOODPASS": (False, 2),
+            "ThisIIISnotaG00dPass": (False, 1)}
 
     for (pwd, expect) in pwds.items():
-        list = [Solution420.minCharacters(pwd), Solution420.domainCheck(pwd), Solution420.repeatCheck(pwd)]
-        assert(expect == all(list))
+        rlist = [Solution420.minCharacters(pwd), Solution420.domainCheck(pwd), Solution420.repeatCheck(pwd)]
+        flags = [x for (x, _) in rlist]
+        errs = sum(y for (_, y) in rlist)
+        assert(expect == (all(flags), errs))
 
 
 def testSolution() -> None:
     pwds = {"this1sAGoodPass": 0,
-            "thisisnotagoodpass": 1,
-            "THISISNOTAGOODPASS": 1,
-            "thisi": 2,
-            "THISI": 2,
-            "TTT": 3,
-            "AAAAAAAAAAAAAAAAAAAAAAAAA": 3,
+            "thisisnotagoodpass": 2,
+            "THISISNOTAGOODPASS": 2,
+            "thisi": 3,
+            "THISI": 3,
+            "TTT": 6,
+            "AAAAAAAAAAAAAAAAAAAAAAAAA": 15,
             "ThisIIISnotaG00dPass": 1}
 
     f = Solution420()
 
     for (pwd, expect) in pwds.items():
-        nErrors = f.strongPasswordChecker(s=pwd)
+        nErrors = f.strongPasswordChecker(pwd)
         assert(expect == nErrors)
         # list = [Solution420.minCharacters(pwd), Solution420.domainCheck(pwd), Solution420.repeatCheck(pwd)]
         # assert(expect == list.count(False))
