5f6d7282f624ebe6756b1bad5e9113c6c639b88d
ketan purohit <ketanpurohit0@gmail.com>
2020-11-25 14:46:50 +0000

    Power Sum wip

--- competitions/src/power_sum.py
+++ competitions/src/power_sum.py
@@ -6,9 +6,10 @@ import math
 
 # Complete the powerSum function below.
 def powerSum(X, N):
-    largestNPower = math.floor(math.pow(X, float(1/N)))
-    seq = [x for x in range(largestNPower, 0, -1)]
-    return powerSumInner(X, N, seq)
+    largestNPower = math.floor(0.0000001 + math.exp(math.log(X)/N))
+    seq = [powerSumInner(X, N, x) for x in range(largestNPower, 0, -1)]
+    # return powerSumInner(X, N, seq)
+    return seq
 
 
 def isWholeRootOfN(X, N):
@@ -19,22 +20,39 @@ def isWholeRootOfN(X, N):
         return True
 
 
-def powerSumInner(X, N, seq):
-    r = []
-    for s in seq:
-        for x in range(s, 0, -1):
-            # print(internal_s)
-            powerval = pow(x, N)
-            remainderval = X - powerval
-            if (remainderval in [0, 1]):
-                r.append((x, powerval, remainderval))
-            else:
-                r.append(powerSum(remainderval, N))
-    return r
+def countWaysUtil(x, n, num):
+
+    # Base cases
+    val = (x - pow(num, n))
+    if (val == 0):
+        return 1
+    if (val < 0):
+        return 0
+
+    # Consider two possibilities, num is
+    # included and num is not included.
+    return countWaysUtil(val, n, num + 1) + countWaysUtil(x, n, num + 1)
+
+
+# Returns number of ways to express
+# x as sum of n-th power of two.
+def countWays(x, n):
+    return countWaysUtil(x, n, 1)
+
+
+def powerSumInner(X, N, x):
+    powerval = pow(x, N)
+    remainderval = X - powerval
+    if (isWholeRootOfN(remainderval, N)):  
+        print(X,N,x,remainderval)
+        return 1
+    else:
+        largestNPower = math.floor(0.0000001 + math.exp(math.log(remainderval)/N))
+        return powerSumInner(remainderval, N, largestNPower)
+
 
 
 if __name__ == '__main__':
-
 
     X = 10
     N = 2
--- competitions/tests/test_power_sum.py
+++ competitions/tests/test_power_sum.py
@@ -2,7 +2,7 @@ import src.power_sum as s
 
 
 def test_is_a_whole_root_n() -> None:
-    for n in range(1, 15):
+    for n in range(0, 15):
         V = [x**n for x in range(15)]
         VV = [s.isWholeRootOfN(x, n) for x in V]
         assert(all(VV))
