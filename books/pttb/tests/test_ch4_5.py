fa40d430f05f8a8fcd143b9dbdfe9729725cbd55
ketan purohit <ketanpurohit0@gmail.com>
2021-04-12 19:32:54 +0100

    Abstract Base Class

---
+++ books/pttb/tests/test_ch4_5.py
@@ -0,0 +1,52 @@
+import pytest
+from abc import ABCMeta, abstractmethod
+
+
+class Base:
+    def foo(self):
+        raise NotImplementedError()
+
+    def bar(self):
+        raise NotImplementedError()
+
+
+class Derived(Base):
+    def foo(self):
+        pass
+
+
+class Base2(metaclass=ABCMeta):
+    @abstractmethod
+    def foo(self):
+        pass
+
+    @abstractmethod
+    def bar(self):
+        pass
+
+
+class Derived2(Base2):
+    def foo(self):
+        pass
+
+
+def test_base():
+    with pytest.raises(NotImplementedError):
+        b = Base()
+        b.foo()
+
+
+def test_derived():
+    with pytest.raises(NotImplementedError):
+        b = Derived()
+        b.bar()
+
+
+def test_base2():
+    with pytest.raises(TypeError):
+        base = Base2()
+
+
+def test_derive2():
+    with pytest.raises(TypeError):
+        derived2 = Derived2()
