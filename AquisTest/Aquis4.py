0dbdf4b47fc3d07777541e3907c80b1a96ec8273
ketan purohit <ketanpurohit0@gmail.com>
2022-02-05 11:12:11 +0000

    Enhancing output

--- AquisTest/Aquis4.py
+++ AquisTest/Aquis4.py
@@ -1,35 +1,50 @@
-import asyncio
+import json
 import time
+from multiprocessing import Process, JoinableQueue
+from queue import Empty
+from threading import Thread
 
 import requests
-from threading import Thread
-from multiprocessing import Process ,JoinableQueue, Pool
-from queue import Empty
+
+from Aquis1 import filterIn, fixJson
 
 
-def downloadFile(sourceFile: str, inboundQueue : JoinableQueue):
+def downloadFile(sourceFile: str, inboundQueue: JoinableQueue):
     streamingReadFromURL = requests.get(sourceFile, stream=True)
     for chunk in streamingReadFromURL.iter_lines(65536):
         inboundQueue.put_nowait(chunk)
 
 
 def worker(name, queue: JoinableQueue):
-    counter : int = 0
-    notEmpty : bool = True
+    counter: int = 0
+    msg8counter: int = 0
+    msg12counter: int = 0
+    msgOthercounter: int = 0
+    notEmpty: bool = True
+
     while notEmpty and queue.qsize():
         try:
             jsonStr = queue.get(block=True, timeout=2.0)
             jsonStr = jsonStr.decode('ASCII')
+            if filterIn(jsonStr):
+                fixedJson = fixJson(jsonStr[2:])
+                jsonObj = json.loads(fixedJson)
+                msgType = jsonObj["header"]["msgType_"]
+                # place the parsed json in relevant containers
+                if msgType == 8:
+                    msg8counter += 1
+                elif msgType == 12:
+                    msg12counter += 1
+                else:
+                    msgOthercounter += 1
             queue.task_done()
-            counter+=1
+            counter += 1
             if counter % 10000 == 0:
-                print(counter, name)
+                print(name, counter, msg8counter, msg12counter, msgOthercounter)
         except Empty:
             notEmpty = False
 
-    print(counter, name)
-
-
+    print(f"{name} completed", counter, msg8counter, msg12counter, msgOthercounter)
 
 
 def useProcess():
@@ -40,23 +55,20 @@ def useProcess():
 
     workers = []
     for i in range(5):
-        w = Process(target=worker, args=(f"worker-{i}",inboundQueue,))
+        w = Process(target=worker, args=(f"worker-{i}", inboundQueue,))
         w.start()
         workers.append(w)
 
-
-
-
     time.sleep(5)
-    print("*",inboundQueue.qsize())
+    print("*", inboundQueue.qsize())
     time.sleep(5)
-    print("*",inboundQueue.qsize())
+    print("*", inboundQueue.qsize())
 
     reader.join()
-    print("*",inboundQueue.qsize())
+    print("*", inboundQueue.qsize())
     inboundQueue.join()
-    print("**",inboundQueue.qsize())
-    map(lambda w: w.join(), workers)
+    print("**", inboundQueue.qsize())
+    map(lambda wi: wi.join(), workers)
 
 
 if __name__ == '__main__':
