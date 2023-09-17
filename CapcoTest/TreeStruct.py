b74f31c91852b716189031f2a4cea34059542f3f
ketan purohit <ketanpurohit0@gmail.com>
2023-09-17 17:44:12 +0100

    Add allocation rate to account node.

--- CapcoTest/TreeStruct.py
+++ CapcoTest/TreeStruct.py
@@ -5,17 +5,19 @@ from typing_extensions import Self
 
 class AccountTree:
     parent_account: Optional[str]
+    allocation_rate: float   # 2dp, must add to 100.00 for all children
     children_accounts: List[Self]
 
-    def __init__(self, account_code: Optional[str]):
+    def __init__(self, account_code: Optional[str], allocation_rate: float):
         self.parent_account: Optional[str] = account_code
+        self.allocation_rate: float = allocation_rate
         self.children_accounts: List[AccountTree] = []
 
-    def insert(self, parent_code: Optional[str], child_account: Optional[str]) -> bool:
+    def insert(self, parent_code: Optional[str], child_account: Optional[str], allocation_rate: float) -> bool:
         tree_root = self.find(parent_code)
         we_need_to_add = tree_root and not tree_root.contains_child(child_account)
         if we_need_to_add:
-            tree_root.children_accounts.append(AccountTree(child_account))
+            tree_root.children_accounts.append(AccountTree(child_account, allocation_rate))
         return we_need_to_add
 
     def find(self, account_code: str) -> Optional[Self]:
@@ -37,3 +39,9 @@ class AccountTree:
         count = 1
         child_counts = sum(ca.node_count() for ca in self.children_accounts)
         return count + child_counts
+
+    def sum_of_immediate_child_allocations(self):
+        """Either sum of child allocations or the rate on the object"""
+        if self.children_accounts:
+            return sum(ca.allocation_rate for ca in self.children_accounts)
+        return self.allocation_rate
--- CapcoTest/tests/test_account_tree.py
+++ CapcoTest/tests/test_account_tree.py
@@ -1,26 +1,28 @@
 import unittest
 
+import numpy as np
+
 from TreeStruct import AccountTree
 
 
 class TestAccountTree(unittest.TestCase):
 
     def test_create_root(self):
-        tree = AccountTree(account_code=None)
+        tree = AccountTree(account_code=None, allocation_rate=1)
         self.assertIsNone(tree.parent_account)
 
     def test_create_non_root(self):
         account_code = "123"
-        tree = AccountTree(account_code=account_code)
+        tree = AccountTree(account_code=account_code, allocation_rate=1)
         self.assertEqual(tree.parent_account, account_code)
 
     def test_insert_at_root(self):
-        tree = AccountTree(account_code=None)
+        tree = AccountTree(account_code=None, allocation_rate=1)
         parent_account, child_account = (None, "123")
-        was_added = tree.insert(parent_account, child_account=child_account)
+        was_added = tree.insert(parent_account, child_account=child_account, allocation_rate=1)
         self.assertTrue(was_added)
-        pa: AccountTree = tree.find(account_code = parent_account)
-        ca: AccountTree = tree.find(account_code = child_account)
+        pa: AccountTree = tree.find(account_code=parent_account)
+        ca: AccountTree = tree.find(account_code=child_account)
         self.assertIsNotNone(pa)
         self.assertIsNotNone(ca)
         self.assertIsNone(pa.parent_account)
@@ -28,51 +30,50 @@ class TestAccountTree(unittest.TestCase):
         self.assertTrue(ca.parent_account, child_account)
 
     def test_insert_where_parent_does_not_exist(self):
-        tree = AccountTree(account_code=None)
+        tree = AccountTree(account_code=None, allocation_rate=1)
         parent_account, child_account = (None, "123")
-        was_added = tree.insert(parent_account, child_account=child_account)
+        was_added = tree.insert(parent_account, child_account=child_account, allocation_rate=1)
         self.assertTrue(was_added)
         parent_account, child_account = ("123f", "123/1")
-        was_added = tree.insert(parent_code=parent_account, child_account=child_account)
+        was_added = tree.insert(parent_code=parent_account, child_account=child_account, allocation_rate=1)
         self.assertFalse(was_added)
 
-
     def test_insert_generally(self):
-        tree = AccountTree(account_code=None)
+        tree = AccountTree(account_code=None, allocation_rate=1)
         parent_account, child_account = (None, "123")
-        _ = tree.insert(parent_account, child_account=child_account)
+        _ = tree.insert(parent_account, child_account=child_account, allocation_rate=1)
         parent_account, child_account = ("123", "123/1")
-        was_added = tree.insert(parent_code=parent_account, child_account=child_account)
+        was_added = tree.insert(parent_code=parent_account, child_account=child_account, allocation_rate=1)
         self.assertTrue(was_added)
         self.assertIsNotNone(tree.find(None))
         self.assertIsNotNone(tree.find("123"))
         self.assertIsNotNone(tree.find("123/1"))
 
     def test_insert_and_find_non_existing(self):
-        tree = AccountTree(account_code=None)
+        tree = AccountTree(account_code=None, allocation_rate=1)
         parent_account, child_account = (None, "123")
-        _ = tree.insert(parent_account, child_account=child_account)
+        _ = tree.insert(parent_account, child_account=child_account, allocation_rate=1)
         parent_account, child_account = ("123", "123/1")
-        was_added = tree.insert(parent_code=parent_account, child_account=child_account)
+        was_added = tree.insert(parent_code=parent_account, child_account=child_account, allocation_rate=1)
         self.assertTrue(was_added)
         self.assertIsNone(tree.find("does_not_exist"))
 
     def test_create_arbitrary_tree(self):
-        tree = AccountTree(None)
+        tree = AccountTree(None, 1)
 
         elements = [
-            (None, "r1"),
-            (None, "r2"),
-            (None, "r3"),
-            (None, "r4"),
-            ("r1", "r1/r1.1"),
-            ("r1", "r1/r1.2"),
-            ("r2", "r2/r2.1"),
-            ("r2/r2.1", "r2/r2.1/r.2.1.1"),
-            ("r2/r2.1", "r2/r2.1/r.2.1.2"),
-            ("r2/r2.1", "r2/r2.1/r.2.1.3"),
-            ("r2/r2.1", "r2/r2.1/r.2.1.4"),
-            ("r2/r2.1", "r2/r2.1/r.2.1.5"),
+            (None, "r1", 25),
+            (None, "r2", 25),
+            (None, "r3", 25),
+            (None, "r4", 25),
+            ("r1", "r1/r1.1", 30),
+            ("r1", "r1/r1.2", 70),
+            ("r2", "r2/r2.1", 100),
+            ("r2/r2.1", "r2/r2.1/r.2.1.1", 11.98),
+            ("r2/r2.1", "r2/r2.1/r.2.1.2", 21.22),
+            ("r2/r2.1", "r2/r2.1/r.2.1.3", 54.76),
+            ("r2/r2.1", "r2/r2.1/r.2.1.4", 6.40),
+            ("r2/r2.1", "r2/r2.1/r.2.1.5", 5.64),
         ]
 
         for e in elements:
@@ -81,9 +82,16 @@ class TestAccountTree(unittest.TestCase):
         nodes = tree.node_count() - 1
         self.assertEqual(len(elements), nodes)
 
+        tree_node = tree.find("r2/r2.1")
+        self.assertIsNotNone(tree_node)
+        total_allocation_rate = tree_node.sum_of_immediate_child_allocations()
+        self.assertTrue(np.isclose(100, total_allocation_rate))
 
-
-
+        for parent_account, _, _ in elements:
+            with self.subTest(parent_account):
+                tree_node = tree.find(account_code=parent_account)
+                total_allocation_rate = tree_node.sum_of_immediate_child_allocations()
+                self.assertTrue(np.isclose(100, total_allocation_rate), msg=parent_account)
 
 
 if __name__ == '__main__':
