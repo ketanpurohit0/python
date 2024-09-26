112385a89d4da65f0d78beb26b2cfd5c9753fd79
kpro-capco <ketan.purohit@capco.com>
2024-09-26 13:31:53 +0100

    Investigate user of Hypothesis and Pydantic model testing

--- CapcoTest/tests/spark_unit_tests.py
+++ CapcoTest/tests/spark_unit_tests.py
@@ -1,18 +1,19 @@
 import unittest
 from typing import Any
 
+from great_expectations.core import ExpectationSuiteSchema, ExpectationSuite
 from pyspark.sql import SparkSession, Row
-from pyspark.sql.functions import col
 from datetime import date, datetime
 import great_expectations as ge
 import json
 
 
 class MySparkTests(unittest.TestCase):
-    def test_dataframe_expectations(self):
-        spark = SparkSession.builder.getOrCreate()
-        spark.sparkContext.setLogLevel("ERROR")
-        df = spark.createDataFrame(
+
+    def setUp(self) -> None:
+        self.spark = SparkSession.builder.getOrCreate()
+        self.spark.sparkContext.setLogLevel("ERROR")
+        self.df = self.spark.createDataFrame(
             [
                 Row(
                     a=1,
@@ -38,13 +39,42 @@ class MySparkTests(unittest.TestCase):
             ]
         )
 
+    def test_dataframe_expectations(self):
+        # spark = SparkSession.builder.getOrCreate()
+        # spark.sparkContext.setLogLevel("ERROR")
+        # df = spark.createDataFrame(
+        #     [
+        #         Row(
+        #             a=1,
+        #             b=2.0,
+        #             c="foo1",
+        #             d=date(2000, 1, 1),
+        #             e=datetime(2000, 1, 1, 12, 0),
+        #         ),
+        #         Row(
+        #             a=2,
+        #             b=3.0,
+        #             c="foo2",
+        #             d=date(2000, 2, 1),
+        #             e=datetime(2000, 1, 2, 12, 0),
+        #         ),
+        #         Row(
+        #             a=4,
+        #             b=5.0,
+        #             c="string3",
+        #             d=date(2000, 3, 1),
+        #             e=datetime(2000, 1, 3, 12, 0),
+        #         ),
+        #     ]
+        # )
+
         context = ge.get_context()
         asset = context.data_sources.add_spark("spark").add_dataframe_asset(
             "data_quality_test"
         )
 
         validator = context.get_validator(
-            batch_request=asset.build_batch_request(options={"dataframe": df})
+            batch_request=asset.build_batch_request(options={"dataframe": self.df})
         )
         result_format = {"result_format": "COMPLETE", "include_unexpected_rows": True}
         result = validator.expect_column_values_to_be_in_set(
@@ -103,14 +133,25 @@ class MySparkTests(unittest.TestCase):
         with open("mytests.pickle", "rb") as fp:
             loaded_suite = pickle.load(fp)
             load_suite_results = validator.validate(expectation_suite=loaded_suite)
-
-        with open("mytests.json") as fp:
-            s = json.load(fp)
-            suite_result = validator.validate(expectation_suite=s)
-        print(result)
+            self.assertTrue(load_suite_results["success"])
 
     def test_from_config(self):
-        pass
+        # use json saved above
+
+        with open("mytests.json") as f:
+            json_dict = json.load(f)
+            expectations_from_json = ExpectationSuite(name="foo", expectations=json_dict['expectations'])
+
+        context = ge.get_context()
+        asset = context.data_sources.add_spark("spark").add_dataframe_asset(
+            "data_quality_test"
+        )
+
+        validator2 = context.get_validator(
+            batch_request=asset.build_batch_request(options={"dataframe": self.df})
+        )
+        suite_result = validator2.validate(expectation_suite=expectations_from_json)
+        self.assertTrue(suite_result["success"])
 
 
 if __name__ == "__main__":
